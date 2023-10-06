6d46cad067561870461a24567d81c9da7d8b7fa4
GarlandNic <nicolas-garland@club-internet.fr>
2023-10-06 14:19:03 +0200

    encodage mot de passe

--- src/main/java/com/openclassrooms/PayMyBuddy/service/PMBUserService.java
+++ src/main/java/com/openclassrooms/PayMyBuddy/service/PMBUserService.java
@@ -3,16 +3,21 @@ package com.openclassrooms.PayMyBuddy.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.openclassrooms.PayMyBuddy.model.PMBUser;
 import com.openclassrooms.PayMyBuddy.repository.PMBUserRepository;
+import com.openclassrooms.PayMyBuddy.security.CustomUserDetailsService;
 
 @Service
 public class PMBUserService {
 	
 	@Autowired
 	private PMBUserRepository userRepo;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder; 
 	
 	public PMBUser createNewUser(PMBUser user) {
 		
@@ -21,6 +26,8 @@ public class PMBUserService {
 		if(existingUser.isPresent()) {
 			return null;
 		} else {
+			// encoder mot de passe
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
 			return userRepo.save(user) ;
 		}
 	}
